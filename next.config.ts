e1a57e4c719c7a6033c745bbfe4120a96a378aba
waipod <waipod@hotmail.com>
2025-07-23 09:11:11 +0700

    hosting2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config: any) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': '.',
+      '@': path.resolve('./'),
     }
     return config
   },
